0ab334f56a1b8a92cb09127db3536fe02cd1ae2a
Ryan-Thrall <ryanthrall04@gmail.com>
2023-08-10 19:07:16 -0600

    First Commit

---
+++ GOPS/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace GOPS.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
